4fe5dfc55c3f3de7ae9cd7f6945a09f387587a4b
valentine <v.shemyako@gmail.com>
2017-12-21 23:00:02 +0300

    Learnt about Comparison interface

---
+++ src/main/java/chapter03/collections/QueueInterface.java
@@ -0,0 +1,14 @@
+package chapter03.collections;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * Collection designed to process elements in specific order
+ */
+public class QueueInterface {
+
+    public static void main(String[] args) {
+        Queue<String> stringQueue = new ArrayDeque<>();
+    }
+}
