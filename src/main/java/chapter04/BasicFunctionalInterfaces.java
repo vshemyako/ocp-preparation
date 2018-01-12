83cf25d3a1616642c7ead5c2c5111e45883a198a
valentine <v.shemyako@gmail.com>
2018-01-12 22:53:48 +0300

    Learnt predicate, bipredicate, function, bifunction, unaryoperator, binaryoperator interfaces

--- src/main/java/chapter04/BasicFunctionalInterfaces.java
+++ src/main/java/chapter04/BasicFunctionalInterfaces.java
@@ -3,9 +3,8 @@ package chapter04;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import java.util.Objects;
+import java.util.function.*;
 
 /**
  * Introduction of basic functional interfaces and their corresponding abstract methods implemented using lambdas and
@@ -14,7 +13,7 @@ import java.util.function.Supplier;
 public class BasicFunctionalInterfaces {
 
     /**
-     * Implements Supplier interface.
+     * Implements {@link Supplier} interface.
      * Supplier's method accepts nothing return object
      */
     public static void supplierExample() {
@@ -26,7 +25,7 @@ public class BasicFunctionalInterfaces {
     }
 
     /**
-     * Shows usage of Consumer interface.
+     * Shows usage of {@link Consumer} interface.
      * Accepts object, return nothing
      */
     public static void consumerExample() {
@@ -38,7 +37,7 @@ public class BasicFunctionalInterfaces {
     }
 
     /**
-     * Shows usage of BiConsumer interface.
+     * Shows usage of {@link BiConsumer} interface.
      * Accepts two objects and return nothing
      */
     public static void biConsumerExample() {
@@ -54,11 +53,81 @@ public class BasicFunctionalInterfaces {
     }
 
     /**
+     * Shows usage of {@link Predicate} interface.
+     * Accepts object and returns boolean value
+     */
+    public static void predicateExample() {
+        Predicate<String> methodReferencePredicate = String::isEmpty;
+        Predicate<String> lambdaPredicate = word -> word.isEmpty();
+
+        System.out.println(methodReferencePredicate.test(""));
+        System.out.println(lambdaPredicate.test(" "));
+    }
+
+    /**
+     * Shows usage of {@link BiPredicate} interface.
+     * Accepts two objects and return boolean value
+     */
+    public static void biPredicateExample() {
+        BiPredicate<String, String> methodReferenceBiPredicate = Objects::equals;
+        BiPredicate<String, String> lambdaBiPredicate = (firstWord, secondWord) -> Objects.equals(firstWord, secondWord);
+
+        System.out.println(methodReferenceBiPredicate.test(new String("one"), new String("one")));
+        System.out.println(lambdaBiPredicate.test(new String("One"), new String("one")));
+    }
+
+    /**
+     * Shows usage of {@link Function} interface.
+     * Accept one object and converts its value or, possibly, into another object
+     */
+    public static void functionExample() {
+        Function<String, Integer> methodReferenceFunction = String::length;
+        Function<String, Integer> lambdaFunction = word -> word.length();
+
+        System.out.println(methodReferenceFunction.apply("Ninety-nine"));
+        System.out.println(lambdaFunction.apply("Ninety-nine"));
+    }
+
+    /**
+     * Shows usage of {@link BiFunction}.
+     * Accept two object and converts them into one
+     */
+    public static void biFunctionExample() {
+        BiFunction<String, String, String> methodReferenceBiFunction = String::concat;
+        BiFunction<String, String, String> lambdaBiFunction = (firstWord, secondWord) -> firstWord.concat(secondWord);
+
+        System.out.println(methodReferenceBiFunction.apply("Hello", "World!"));
+        System.out.println(lambdaBiFunction.apply("Hello", "World!"));
+    }
+
+    /**
+     * {@link UnaryOperator} example
+     */
+    public static void unaryOperatorExample() {
+        UnaryOperator<String> methodReferenceUnaryOperator = String::toUpperCase;
+        System.out.println(methodReferenceUnaryOperator.apply("nope"));
+    }
+
+    /**
+     * {@link BinaryOperator} example
+     */
+    public static void binaryOperatorExample() {
+        BinaryOperator<String> methodReferenceBinaryOperator = String::concat;
+        System.out.println(methodReferenceBinaryOperator.apply("yea", "yes"));
+    }
+
+    /**
      * Fires up all interfaces implementations
      */
     public static void main(String[] args) {
         supplierExample();
         consumerExample();
         biConsumerExample();
+        predicateExample();
+        biPredicateExample();
+        functionExample();
+        biFunctionExample();
+        unaryOperatorExample();
+        binaryOperatorExample();
     }
 }
