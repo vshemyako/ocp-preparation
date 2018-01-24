0d727d4a086a0c21a7b8445d50f741549b6b1166
valentine <v.shemyako@gmail.com>
2018-01-24 22:38:19 +0300

    Learnt about autocloseable interface

--- src/main/java/chapter06/MutliCatchClose.java
+++ src/main/java/chapter06/MutliCatchClose.java
@@ -1,8 +1,5 @@
 package chapter06;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 /**
  * Sometimes it's necessary to catch several exceptions types which are not from the same hierarchy, but which are
  * related to a common problem. JDK1.7 present new approach to reduce extra code for doing this
@@ -17,24 +14,30 @@ public class MutliCatchClose {
         }
 
         //illegal syntax
+        /*
         try {
             //do something
         } catch (NullPointerException ex,IllegalArgumentException ex){
             System.out.println("Only one local variable shared among exception types!");
         }
+        */
 
         //illegal syntax
+        /*
         try {
             //do something
         } catch (NullPointerException ex1,IllegalArgumentException ex2, ArrayIndexOutOfBoundsException ex3){
             System.out.println("Only one local variable shared among exception types!");
         }
+        */
 
         //types have to be from different exception hierarchies
+        /*
         try {
             //do something
         } catch (FileNotFoundException,IOException ex){
             System.out.println("No hierarchical relation between exception types!");
         }
+        */
     }
 }
