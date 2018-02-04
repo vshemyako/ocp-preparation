ed0f8baa99b551bc46200e566b864a1e2e7b9c90
valentine <v.shemyako@gmail.com>
2018-02-04 12:29:40 +0300

    Fixed unclosed streams issue

--- src/main/java/chapter08/BasicByteStreamUsage.java
+++ src/main/java/chapter08/BasicByteStreamUsage.java
@@ -16,8 +16,7 @@ public class BasicByteStreamUsage {
     private static void markPosition() {
         File textFile = new File("C:\\Projects\\Certification\\OCPJP\\ocp-preparation\\src\\main\\java\\chapter08\\abc.txt");
         if (textFile.exists()) {
-            try {
-                BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(textFile));
+            try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(textFile));) {
                 //have to check if our input stream instance support desired method
                 if (inputStream.markSupported()) {
                     int readLimit = 10;
@@ -38,9 +37,27 @@ public class BasicByteStreamUsage {
     }
 
     /**
+     * Sometimes you need to skip several bytes in order not to output them
+     */
+    private static void skipBytes() {
+        File textFile = new File("C:\\Projects\\Certification\\OCPJP\\ocp-preparation\\src\\main\\java\\chapter08\\abc.txt");
+        //lets print the first and the last letters from ABC
+        try (FileInputStream inputStream = new FileInputStream(textFile);) {
+            System.out.println((char) inputStream.read()); //A
+            long bytesToSkip = 24;
+            long actualNumber = inputStream.skip(bytesToSkip);
+            System.out.println((char) inputStream.read()); //Z
+            System.out.println("Number of bytes skipped: " + actualNumber);
+        } catch (IOException e) {
+            //handle to rethrow
+        }
+    }
+
+    /**
      * Entry point
      */
     public static void main(String[] args) {
-        markPosition();
+        //markPosition();
+        skipBytes();
     }
 }
