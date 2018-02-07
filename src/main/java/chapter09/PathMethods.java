85d400e9b88ffe7d5b920f95f95900ac5933b25f
valentine <v.shemyako@gmail.com>
2018-02-07 22:40:26 +0300

    Path's elements manipulation

---
+++ src/main/java/chapter09/PathMethods.java
@@ -0,0 +1,32 @@
+package chapter09;
+
+import java.io.Console;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * 10.000 feet examination of {@link java.nio.file.Path} interface methods
+ */
+public class PathMethods {
+
+    public static void main(String[] args) {
+        Path path = Paths.get("C:\\Projects\\Certification\\OCPJP\\ocp-preparation\\src\\main\\java\\chapter08\\abc_copied.txt");
+
+        //Basic path info
+        Console console = System.console();
+        PrintWriter writer = console.writer();
+        writer.println("Path is:");
+        writer.println(path.toString());
+
+        //Iterating over a path object
+        //Pay attention that root element is not included
+        path.forEach(writer::println);
+
+        //It's possible to achieve the same result in more complex way
+        writer.println("More complex way:");
+        for(int index = 0; index < path.getNameCount(); index++) {
+            writer.println(path.getName(index));
+        }
+    }
+}
